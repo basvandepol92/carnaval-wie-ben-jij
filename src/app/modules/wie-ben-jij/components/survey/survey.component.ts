9e476607363b8ff9029e63cc4d29b8017bdf73b5
Bas van de Pol <bas.van.de.pol@rabobank.nl>
2022-02-11 23:14:11 +0100

    Text fixes

--- src/app/modules/wie-ben-jij/components/survey/survey.component.ts
+++ src/app/modules/wie-ben-jij/components/survey/survey.component.ts
@@ -17,12 +17,12 @@ export class SurveyComponent {
   selectedGroup = [];
   selectedPerson: number = 0;
 
-  question_1: Question = { question: 'Welke snack eet jij met carnaval?', answer_1: 'Frikandel maar wel een speciale' , answer_2: 'Balletjes van de koniging', answer_3: 'Falaffel in mn waffel', answer_4: 'Heel veel bruine boterhammen' };
+  question_1: Question = { question: 'Welke snack eet jij met carnaval?', answer_1: 'Frikandel maar wel een speciale' , answer_2: 'Balletjes van de koningin', answer_3: 'Falafel in mn waffel', answer_4: 'Heel veel bruine boterhammen' };
   question_2: Question = { question: 'Wie is je favoriete artiest?', answer_1: 'Lamme Frans', answer_2: 'Vieze Jack', answer_3: 'Jettie Trompettie', answer_4: 'Het Feestteam' };
   question_3: Question = { question: 'Hoe ga jij verkleed?', answer_1: 'In mn scoutingblouse', answer_2: 'Als the voice of holland coach', answer_3: 'Als piloot', answer_4: 'Ik ga niet verkleed' };
-  question_4: Question = { question: 'Wat is jouw favoriete carnavals nummer?', answer_1: 'Zak es lekker door', answer_2: 'Er staat een paard in de gang', answer_3: 'Bloemetjes gordijn', answer_4: 'Potentie' };
+  question_4: Question = { question: 'Wat is jouw favoriete carnavals nummer?', answer_1: 'Zak es lekker door', answer_2: 'Er staat een paard in de gang', answer_3: 'Bloemetjesgordijn', answer_4: 'Potentie' };
   question_5: Question = { question: 'Wat doe jij het liefste met carnaval?', answer_1: 'Naar dn opstoet kijken natuurlijk!', answer_2: 'Bier drinken, wa dan?', answer_3: 'Thuis blijven, mij niet gezien!', answer_4: 'Wintersporteuhhh' };
-  question_6: Question = { question: 'Bij welke carnavalvereniging zou jij zitten?', answer_1: 'CV de padvinders', answer_2: 'CV de blauw wit uilen', answer_3: 'CV weg er mee', answer_4: 'CV experience' };
+  question_6: Question = { question: 'Bij welke carnavalsvereniging zou jij zitten?', answer_1: 'CV de padvinders', answer_2: 'CV de blauw wit uilen', answer_3: 'CV weg er mee', answer_4: 'CV experience' };
   question_7: Question = { question: 'Hoelaat ga jij naar bed met carnaval?', answer_1: 'Niet! Slapen is voor apen', answer_2: 'Net zo laat als normaal', answer_3: 'Ik doe het licht uit in de kroeg', answer_4: 'Zodra ik moe ben natuurlijk' };
   question_8: Question = { question: 'In welke stad vier jij carnaval?', answer_1: 'Ballefruttersgat', answer_2: 'Kruikenstad', answer_3: 'Oeteldonk', answer_4: 'Kraaiengat' };
   question_9: Question = { question: 'In welke kroeg vier jij carnaval?', answer_1: 'Café t mandje', answer_2: 'Kelderke', answer_3: 'Brands', answer_4: 'Op straat' };
