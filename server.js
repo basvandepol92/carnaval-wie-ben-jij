b6de2c23319ac7a9314f9fa6b6bb75fdaac16009
Bas van de Pol <bas.van.de.pol@rabobank.nl>
2022-02-11 23:06:35 +0100

    fix for server

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ const app = express();
 
 app.use(express.static(__dirname + '/dist/carnaval-wie-ben-jij'));
 
-app.get('/*', (req,res,next) => {
-    res.sendFile(path.join(__dirname + '/dist/carnaval-wie-ben-jij/index.html'));
+app.get('/*', function (req, res) {
+    res.sendFile('index.html', { root: 'dist/carnaval-wie-ben-jij/' }
+    );
 });
-
 
 app.listen(process.env.PORT || 8000);
